57f48a27cc6110d8e3fc506630399c2ccc578dbb
zerosimonn <de.regt.simon@gmail.com>
2024-12-31 21:43:38 +0100

    pygame screen

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+   # print("Starting asteroids!")
+   # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        #terminate with x button
+        # terminate with x button
 
         screen.fill(0,None,0)
-        #fill screen black
-
+        # fill screen black
+        # screen.fill("black")
         pygame.display.flip()
-        #refresh screen
+        # refresh screen
 
 if __name__ == "__main__":
     main()
