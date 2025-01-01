c71fd55d849a68b101603e9cd86a9188c4ec8af5
zerosimonn <de.regt.simon@gmail.com>
2025-01-01 14:37:01 +0100

    Turn player

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
-   # print("Starting asteroids!")
-   # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+#    print("Starting asteroids!")
+#    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # terminate with x button
+        player.update(dt)
 
         screen.fill(0,None,0)
-        # fill screen black
         # screen.fill("black")
-        pygame.display.flip()
-        # refresh screen
+        
+        player.draw(screen)
 
+        pygame.display.flip()
+        
+        clock.tick(60)
+        
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
 
